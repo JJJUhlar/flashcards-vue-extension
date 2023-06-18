3f82ccdeac355f6c64f013a274ffa0eee25bc094
JJJUhlar <jjuhlar@gmail.com>
2023-06-18 17:47:10 +0100

    removed login panel

--- src/components/PanelsContainer.vue
+++ src/components/PanelsContainer.vue
@@ -1,37 +1,27 @@
 <script lang="ts">
 import CreateFlashcardsPanel from './CreateFlashcardsPanel.vue';
 import ReviewFlashcardsPanel from './ReviewFlashcardsPanel.vue';
-import LoginPanel from './LoginPanel.vue';
 
 export default {
     data() {
         return {
             showReviewPanel: true,
-            showCreatePanel: false,
-            showLoginPanel: false
+            showCreatePanel: false
         }
     },
     methods: {
         changeToCreatePanel() {
             this.showCreatePanel = true;
             this.showReviewPanel = false;
-            this.showLoginPanel = false;
         },
         changeToReviewPanel() {
             this.showReviewPanel = true;
-            this.showLoginPanel = false;
-            this.showCreatePanel = false;
-        },
-        changeToLoginPanel() { 
-            this.showLoginPanel = true;
-            this.showReviewPanel = false;
             this.showCreatePanel = false;
         }
     },
     components: {
         CreateFlashcardsPanel,
-        ReviewFlashcardsPanel,
-        LoginPanel
+        ReviewFlashcardsPanel
     }
 }
 
@@ -41,7 +31,6 @@ export default {
     <div>
         <button @click="changeToReviewPanel">Review Panel</button>
         <button @click="changeToCreatePanel">Create Panel</button>
-        <button @click="changeToLoginPanel">Login Panel</button>
     </div>
     <div v-if="showCreatePanel">
         <h1>Create Cards</h1>
@@ -51,11 +40,6 @@ export default {
         <h1>Review Cards</h1>
         <ReviewFlashcardsPanel />
     </div>
-    <div v-if="showLoginPanel">
-        <h1>Login</h1>
-        <LoginPanel />
-    </div>
-
 
 </template>
 
