ceb497d4696bdf08305263bdcd9f019d87f5772c
JJJUhlar <jjuhlar@gmail.com>
2023-07-04 16:42:06 +0100

    ts definition for loading spinner

---
+++ src/spinner.d.ts
@@ -0,0 +1,6 @@
+declare module 'vue-spinner/src/ClipLoader.vue' {
+    import { DefineComponent } from 'vue';
+    const component: DefineComponent<{}, {}, any>;
+    export default component;
+  }
+  
